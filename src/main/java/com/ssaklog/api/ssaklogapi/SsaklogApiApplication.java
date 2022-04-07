ce21de231f5f687ac6f22b94fb53a71d79e6761a
seulhee <lsh9412@yanadoocorp.com>
2022-04-07 18:29:33 +0900

    first commit

---
+++ src/main/java/com/ssaklog/api/ssaklogapi/SsaklogApiApplication.java
@@ -0,0 +1,13 @@
+package com.ssaklog.api.ssaklogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsaklogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsaklogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssaklog/api/ssaklogapi/SsaklogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssaklog.api.ssaklogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsaklogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
