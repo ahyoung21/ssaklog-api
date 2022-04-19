f8d5982732386044d89a69fe3fcaf27fc70adc8f
seulhee <lsh9412@yanadoocorp.com>
2022-04-19 19:08:02 +0900

    controller test

---
+++ src/main/java/com/ssaklog/api/ssaklogapi/api/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.ssaklog.api.ssaklogapi.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+@RequiredArgsConstructor
+public class HomeController {
+
+    @GetMapping("")
+    public String home() {
+        return "Hello !!NEW!! SsakLog";
+    }
+
+}
