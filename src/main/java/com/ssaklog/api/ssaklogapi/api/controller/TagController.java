10aab1ffaaa5af69db3465e57a0040c3d9072277
ehko <eunhea2186@yanadoo.co.kr>
2022-05-11 12:10:44 +0900

    Swagger 세팅

--- src/main/java/com/ssaklog/api/ssaklogapi/api/controller/TagController.java
+++ src/main/java/com/ssaklog/api/ssaklogapi/api/controller/TagController.java
@@ -2,6 +2,7 @@ package com.ssaklog.api.ssaklogapi.api.controller;
 
 import com.ssaklog.api.ssaklogapi.api.entity.Tag;
 import com.ssaklog.api.ssaklogapi.api.service.TagService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ public class TagController {
 
     private final TagService tagService;
 
+    @ApiOperation(value = "태그 정보 리스트 조회", response = Tag.class, responseContainer = "List")
     @GetMapping("/list")
     public List<Tag> list() {
         List<Tag> list = tagService.listTags(); // 디버깅을 위해 분리
--- src/main/java/com/ssaklog/api/ssaklogapi/api/entity/Tag.java
+++ src/main/java/com/ssaklog/api/ssaklogapi/api/entity/Tag.java
@@ -1,16 +1,29 @@
 package com.ssaklog.api.ssaklogapi.api.entity;
 
+import com.sun.istack.NotNull;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "TAG")
+@ApiModel(value = "태그 정보")
 public class Tag {
 
     @Id
+    @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(value = "태그 시퀀스")
     private int tagSeq;
 
     @Column
+    @NotNull
+    @ApiModelProperty(value = "태그 명")
     private String tagName;
 
 }
