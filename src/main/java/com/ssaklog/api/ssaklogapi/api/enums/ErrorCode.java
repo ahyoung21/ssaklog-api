7f26be217395a3427df3372a8ac92f6a4b679a8a
seulhee <lsh9412@yanadoocorp.com>
2022-07-08 17:00:52 +0900

    회원 에러 코드 추가

--- src/main/java/com/ssaklog/api/ssaklogapi/api/enums/ErrorCode.java
+++ src/main/java/com/ssaklog/api/ssaklogapi/api/enums/ErrorCode.java
@@ -9,7 +9,13 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     /* 400 BAD_REQUEST : 잘못된 요청 */
-    EMPTY_USER_ID(HttpStatus.BAD_REQUEST, "아이디를 입력해주세요.");
+    EMPTY_USER_ID(HttpStatus.BAD_REQUEST, "아이디를 입력해주세요."),
+    EMPTY_USER_NAME(HttpStatus.BAD_REQUEST, "이름을 입력해주세요."),
+    EMPTY_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호를 입력해주세요."),
+    EMPTY_BIRTHDAY(HttpStatus.BAD_REQUEST, "생년월일을 입력해주세요."),
+    EMPTY_EMAIL(HttpStatus.BAD_REQUEST, "이메일을 입력해주세요."),
+    EMPTY_CERTIFICATE_NUMBER(HttpStatus.BAD_REQUEST, "인증번호를 입력해주세요")
+    ;
 
     private final HttpStatus httpStatus;
     private final String message;
