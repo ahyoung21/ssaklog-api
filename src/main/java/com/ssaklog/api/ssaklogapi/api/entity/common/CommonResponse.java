983305b3bdcb02d3a322c601c25f88c49c1a8418
seulhee <lsh9412@yanadoocorp.com>
2022-07-11 16:35:15 +0900

    CommonResponse 타입 수정

--- src/main/java/com/ssaklog/api/ssaklogapi/api/entity/common/CommonResponse.java
+++ src/main/java/com/ssaklog/api/ssaklogapi/api/entity/common/CommonResponse.java
@@ -2,64 +2,80 @@ package com.ssaklog.api.ssaklogapi.api.entity.common;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.ssaklog.api.ssaklogapi.api.enums.ResponseEnum;
-import lombok.Getter;
 
-@Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class CommonResponse {
+public class CommonResponse<T> {
+
     private CommonResponseMeta meta;
-    private Object data;
 
-    public static CommonResponse fail() {
-        return new CommonResponse(ResponseEnum.FAIL.getCode(), ResponseEnum.FAIL.getValue());
-    }
-
-    public static CommonResponse failOf(String errorMessage) {
-        return new CommonResponse(ResponseEnum.FAIL.getCode(), errorMessage);
-    }
-
-    public static CommonResponse failOf(int code, String errorMessage) {
-        return new CommonResponse(code, errorMessage);
-    }
-
-    public static CommonResponse failOf(String errorMessage, String path) {
-        return new CommonResponse(new CommonResponseMeta(ResponseEnum.FAIL.getCode(), errorMessage, path));
-    }
-
-    public static CommonResponse failOf(int code, String errorMessage, String path) {
-        return new CommonResponse(new CommonResponseMeta(code, errorMessage, path));
-    }
+    private T data;
 
     public CommonResponse() {
+
         setSuccess();
-        this.data = new EmptyResult();
+        this.data = null;
     }
 
-    public CommonResponse(Object result) {
+    public CommonResponse(T result) {
+
         setSuccess();
         this.data = result;
     }
 
     public CommonResponse(int code, String message) {
+
         this.meta = new CommonResponseMeta(code, message);
-        this.data = new EmptyResult();
+        this.data = null;
     }
 
-    public CommonResponse(int code, String message, Object result) {
+    public CommonResponse(int code, String message, T result) {
+
         this.meta = new CommonResponseMeta(code, message);
         this.data = result;
     }
 
-    public CommonResponse(CommonResponseMeta meta, Object data) {
+    public CommonResponse(CommonResponseMeta meta) {
+
         this.meta = meta;
-        this.data = data;
     }
 
-    public CommonResponse(CommonResponseMeta meta) {
-        this.meta = meta;
+    public static CommonResponse fail() {
+
+        return new CommonResponse(ResponseEnum.FAIL.getCode(), ResponseEnum.FAIL.getValue());
+    }
+
+    public static CommonResponse failOf(String errorMessage) {
+
+        return new CommonResponse(ResponseEnum.FAIL.getCode(), errorMessage);
+    }
+
+    public static CommonResponse failOf(int code, String errorMessage) {
+
+        return new CommonResponse(code, errorMessage);
+    }
+
+    public static CommonResponse failOf(String errorMessage, String path) {
+
+        return new CommonResponse(new CommonResponseMeta(ResponseEnum.FAIL.getCode(), errorMessage, path));
+    }
+
+    public static CommonResponse failOf(int code, String errorMessage, String path) {
+
+        return new CommonResponse(new CommonResponseMeta(code, errorMessage, path));
+    }
+
+    public CommonResponseMeta getMeta() {
+
+        return meta;
+    }
+
+    public T getData() {
+
+        return data;
     }
 
     private void setSuccess() {
+
         this.meta = new CommonResponseMeta(ResponseEnum.SUCCESS.getCode(), ResponseEnum.SUCCESS.getValue());
     }
 }
--- src/main/java/com/ssaklog/api/ssaklogapi/api/entity/common/CommonResponseMeta.java
+++ src/main/java/com/ssaklog/api/ssaklogapi/api/entity/common/CommonResponseMeta.java
@@ -1,56 +1,72 @@
 package com.ssaklog.api.ssaklogapi.api.entity.common;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
 
 import java.net.InetAddress;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
-@Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommonResponseMeta {
     private int code;
+
     private String message;
+
     private String path;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dateTime;
 
     @JsonProperty("sId")
     private String sId; // 서버 IP 마지막 자리
 
+    protected CommonResponseMeta() {
+
+    }
+
     public CommonResponseMeta(int code, String message) {
+
         setMeta(code, message);
     }
 
     public CommonResponseMeta(int code, String message, String path) {
+
         setMeta(code, message);
         this.path = path;
     }
 
-    @JsonCreator
-    public CommonResponseMeta(
-            @JsonProperty("code") int code,
-            @JsonProperty("message") String message,
-            @JsonProperty("dateTime") String dateTime,
-            @JsonProperty("path") String path,
-            @JsonProperty("sId") String sId) {
-        this.code = code;
-        this.message = message;
-        this.path = path;
-        this.dateTime = LocalDateTime.parse(dateTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        this.sId = sId;
+    public int getCode() {
+
+        return code;
+    }
+
+    public String getMessage() {
+
+        return message;
+    }
+
+    public String getPath() {
+
+        return path;
+    }
+
+    public LocalDateTime getDateTime() {
+
+        return dateTime;
     }
 
     private void setMeta(int code, String message) {
+
         this.code = code;
         this.message = message;
         this.dateTime = LocalDateTime.now();
         this.sId = getSid();
     }
 
+    @Deprecated
     private String getSid() {
+
         String sid = "";
         try {
             String serverIp = InetAddress.getLocalHost().getHostAddress();
